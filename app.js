744f40e2bf1c207d9cf1c3d7168e9777275085e0
yopse <82827725+yopse@users.noreply.github.com>
2021-12-08 17:56:04 +0530

    all updation done

--- app.js
+++ app.js
@@ -6,55 +6,50 @@ var checkButton = document.querySelector("#check-button");
 
 var message = document.querySelector("#error-message");
 
-checkButton.addEventListener('click',function validateBillCashAmount()
-{
-     hideMessage();
+var availableNotes = document.querySelectorAll(".no-of-notes");
 
-    if(billAmount.value>0)
-    {
+var availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
-      if(cashGiven.value >= billAmount.value ) {
+//Major processing
 
-        var amountToBeReturned = cashGiven.value- billAmount.value;
+checkButton.addEventListener("click", function validateBillCashAmount() {
+  hideMessage();
 
-        calculateChange(amountToBeReturned);
+  if (billAmount.value > 0) {
+    if (cashGiven.value >= billAmount.value) {
+      var amountToBeReturned = cashGiven.value - billAmount.value;
 
-      }
-      else{
-            
-         showMessage("The cash provided should atleast be equal to the bill amount");
-
-      }
-
+      calculateChange(amountToBeReturned);
+    } else {
+      showMessage(
+        "The cash provided should atleast be equal to the bill amount"
+      );
     }
-    else {
-          
-          showMessage('Invalid Bill Amount');
+  } else {
+    showMessage("Invalid Bill Amount");
+  }
+});
 
-    }
+//calculation checks
 
+function calculateChange(amountToBeReturned) {
+  //Itervation over all the given values
+  for (let i = 0; availableNotes.length; i++) {
+    var numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
 
+    amountToBeReturned %= availableNotes[0];
 
-})
-
-function calculateChange()
-{
-     
+    noOfNotes[i].innerText = numberOfNotes;
+  }
 }
-
 
 function hideMessage() {
-
-    message.style.display = "none";
-
+  message.style.display = "none";
 }
-
 
 //showMessage
 
 function showMessage(msg) {
-
-  message .style.display="block";
-  message.innerText=msg;
-    
+  message.style.display = "block";
+  message.innerText = msg;
 }
